8c824ddccca95f4d9b2da29152afabd8bd9d528b
Alexey Demin <AlexeyDeminA@gmail.com>
2026-03-09 03:09:03 +0300

    feat: add kernels in backend

--- aifw/core/backend/cpu_backend.hpp
+++ aifw/core/backend/cpu_backend.hpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <new>
 
+#include "../ops/cpu_kernel_registry.hpp"
 #include "backend.hpp"
 
 namespace aifw::core {
@@ -11,8 +12,12 @@ class CpuBackend final : public IBackend {
  public:
   void* allocate(size_t bytes) override;
   void deallocate(void* ptr) override;
-
   void memcpy(void* dst, const void* src, size_t bytes) override;
+
+  IKernelRegistry& kernels() override;
+
+ private:
+  CpuKernelRegistry kernels_;
 };
 
 inline void* CpuBackend::allocate(size_t bytes) {
@@ -26,5 +31,7 @@ inline void CpuBackend::deallocate(void* ptr) {
 inline void CpuBackend::memcpy(void* dst, const void* src, size_t bytes) {
   std::memcpy(dst, src, bytes);
 }
+
+inline IKernelRegistry& CpuBackend::kernels() { return kernels_; }
 
 }  // namespace aifw::core
