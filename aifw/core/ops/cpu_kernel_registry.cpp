f341b120ec39a68546bf41aaa0a298c7d01fde9a
Alexey Demin <AlexeyDeminA@gmail.com>
2026-03-19 00:33:48 +0300

    feat: add strided ops for cpu kernels

--- aifw/core/ops/cpu_kernel_registry.cpp
+++ aifw/core/ops/cpu_kernel_registry.cpp
@@ -11,6 +11,7 @@ namespace aifw::core {
 void CpuKernelRegistry::matmul(const Tensor& a, const Tensor& b, Tensor& out) {
   AIFW_ASSERT(a.shape().rank() == 2);
   AIFW_ASSERT(b.shape().rank() == 2);
+  AIFW_ASSERT(out.shape().rank() == 2);
   AIFW_ASSERT(a.shape()[1] == b.shape()[0]);
 
   std::memset(out.data(), 0, out.numel() * dtype_size(out.dtype()));
@@ -20,14 +21,24 @@ void CpuKernelRegistry::matmul(const Tensor& a, const Tensor& b, Tensor& out) {
     const size_t K = a.shape()[1];
     const size_t N = b.shape()[1];
 
-    const auto* pa = a.data_as<T>();
-    const auto* pb = b.data_as<T>();
-    T* po = out.data_as<T>();
+    const size_t sa0 = a.stride()[0];
+    const size_t sa1 = a.stride()[1];
+
+    const size_t sb0 = b.stride()[0];
+    const size_t sb1 = b.stride()[1];
+
+    const size_t so0 = out.stride()[0];
+    const size_t so1 = out.stride()[1];
+
+    const auto* pa = static_cast<const T*>(a.data()) + a.offset();
+    const auto* pb = static_cast<const T*>(b.data()) + b.offset();
+    T* po = static_cast<T*>(out.data()) + out.offset();
 
     for (size_t m = 0; m < M; ++m) {
       for (size_t k = 0; k < K; ++k) {
-        const T a_mk = pa[m * K + k];
-        for (size_t n = 0; n < N; ++n) po[m * N + n] += a_mk * pb[k * N + n];
+        const T a_mk = pa[m * sa0 + k * sa1];
+        for (size_t n = 0; n < N; ++n)
+          po[m * so0 + n * so1] += a_mk * pb[k * sb0 + n * sb1];
       }
     }
   });
--- aifw/core/ops/cpu_kernel_registry.hpp
+++ aifw/core/ops/cpu_kernel_registry.hpp
@@ -4,6 +4,8 @@
 
 #include "../assert.hpp"
 #include "../tensor/tensor.hpp"
+#include "../tensor/tensor_iterator.hpp"
+#include "dispatch.hpp"
 #include "kernel_registry.hpp"
 
 namespace aifw::core {
@@ -20,6 +22,16 @@ class CpuKernelRegistry final : public IKernelRegistry {
  private:
   template <typename Op>
   static void elementwise(const Tensor& a, const Tensor& b, Tensor& out, Op op);
+
+  template <typename Op>
+  static void elementwise_contiguous(
+      const Tensor& a, const Tensor& b, Tensor& out, Op op
+  );
+
+  template <typename Op>
+  static void elementwise_strided(
+      const Tensor& a, const Tensor& b, Tensor& out, Op op
+  );
 };
 
 inline void CpuKernelRegistry::add(
@@ -47,15 +59,37 @@ inline void CpuKernelRegistry::div(
 }
 
 inline void CpuKernelRegistry::relu(const Tensor& a, Tensor& out) {
-  dtype_dispatch(a.dtype(), [&]<typename T>() {
-    const T* pa = a.data_as<T>();
-    T* po = out.data_as<T>();
-    for (size_t i = 0; i < a.numel(); ++i) po[i] = pa[i] > T{0} ? pa[i] : T{0};
-  });
+  if (detail::resolve_path(a, out) == detail::ExecPath::Contiguous) {
+    dtype_dispatch(a.dtype(), [&]<typename T>() {
+      const T* pa = a.data_as<T>();
+      T* po = out.data_as<T>();
+      for (size_t i = 0; i < a.numel(); ++i)
+        po[i] = pa[i] > T{0} ? pa[i] : T{0};
+    });
+  } else {
+    dtype_dispatch(a.dtype(), [&]<typename T>() {
+      TensorIterator ia(a);
+      TensorIterator io(out);
+      const T* base_a = static_cast<const T*>(a.data());
+      T* base_o = static_cast<T*>(out.data());
+      for (size_t i = 0; i < a.numel(); ++i)
+        base_o[io[i]] = base_a[ia[i]] > T{0} ? base_a[ia[i]] : T{0};
+    });
+  }
 }
 
 template <typename Op>
 void CpuKernelRegistry::elementwise(
+    const Tensor& a, const Tensor& b, Tensor& out, Op op
+) {
+  if (detail::resolve_path(a, b, out) == detail::ExecPath::Contiguous)
+    elementwise_contiguous(a, b, out, op);
+  else
+    elementwise_strided(a, b, out, op);
+}
+
+template <typename Op>
+void CpuKernelRegistry::elementwise_contiguous(
     const Tensor& a, const Tensor& b, Tensor& out, Op op
 ) {
   AIFW_ASSERT(a.numel() == b.numel());
@@ -65,6 +99,24 @@ void CpuKernelRegistry::elementwise(
     const T* pb = b.data_as<T>();
     T* po = out.data_as<T>();
     for (size_t i = 0; i < a.numel(); ++i) po[i] = op(pa[i], pb[i]);
+  });
+}
+
+template <typename Op>
+void CpuKernelRegistry::elementwise_strided(
+    const Tensor& a, const Tensor& b, Tensor& out, Op op
+) {
+  AIFW_ASSERT(a.numel() == b.numel());
+
+  dtype_dispatch(a.dtype(), [&]<typename T>() {
+    TensorIterator ia(a);
+    TensorIterator ib(b);
+    TensorIterator io(out);
+    const T* base_a = static_cast<const T*>(a.data());
+    const T* base_b = static_cast<const T*>(b.data());
+    T* base_o = static_cast<T*>(out.data());
+    for (size_t i = 0; i < a.numel(); ++i)
+      base_o[io[i]] = op(base_a[ia[i]], base_b[ib[i]]);
   });
 }
 
