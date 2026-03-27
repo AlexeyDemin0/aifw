b896ddc7373175d464df906b49403e0efc692171
Alexey Demin <AlexeyDeminA@gmail.com>
2026-03-28 02:21:23 +0300

    feat: add read/write_bytes to idevice

--- aifw/core/device/idevice.hpp
+++ aifw/core/device/idevice.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstddef>
+
 #include "aifw/core/device/device.hpp"
 #include "aifw/core/device/iallocator.hpp"
 #include "aifw/core/device/ikernel_registry.hpp"
@@ -13,6 +15,13 @@ class IDevice {
   virtual Device device() const = 0;
   virtual IAllocator& allocator() = 0;
   virtual IKernelRegistry& kernels() = 0;
+
+  virtual void read_bytes(
+      void* dst_host, const void* src_device, size_t bytes
+  ) = 0;
+  virtual void write_bytes(
+      void* dst_device, const void* src_host, size_t bytes
+  ) = 0;
 };
 
 }  // namespace aifw::core
--- aifw/core/runtime/cpu/cpu_device.hpp
+++ aifw/core/runtime/cpu/cpu_device.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstring>
+
 #include "aifw/core/device/iallocator.hpp"
 #include "aifw/core/device/idevice.hpp"
 #include "aifw/core/device/ikernel_registry.hpp"
@@ -13,6 +15,17 @@ class CpuDevice final : public IDevice {
   Device device() const override { return kCpu; }
   IAllocator& allocator() override { return allocator_; }
   IKernelRegistry& kernels() override { return kernels_; }
+
+  void read_bytes(
+      void* dst_host, const void* src_device, size_t bytes
+  ) override {
+    std::memcpy(dst_host, src_device, bytes);
+  }
+  void write_bytes(
+      void* dst_device, const void* src_host, size_t bytes
+  ) override {
+    std::memcpy(dst_device, src_host, bytes);
+  }
 
  private:
   CpuAllocator allocator_;
